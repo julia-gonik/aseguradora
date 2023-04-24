3755f62e7333d5c3b070882f19199124c6dd79e0
Calvo <matias.calvo.2000@gmail.com>
2023-04-23 21:31:05 -0300

    Falta implementar los repo

--- Aseguradora.Aplicacion/ListarTitularesConSusVehiculosUseCase.cs
+++ Aseguradora.Aplicacion/ListarTitularesConSusVehiculosUseCase.cs
@@ -3,18 +3,25 @@ namespace Aseguradora.Aplicacion
 {
       public class ListarTitularesConSusVehiculosUseCase
     {
-        private readonly IRepositorioPoliza _repo;
+        private readonly IRepositorioVehiculo _repoVehiculo;
+        private readonly IRepositorioTitular _repoTitular;
 
-        public ListarTitularesConSusVehiculosUseCase(IRepositorioPoliza repo)
+        public ListarTitularesConSusVehiculosUseCase(IRepositorioVehiculo repoVehiculo, IRepositorioTitular repoTitular)
         {
-            _repo = repo;
+            _repoVehiculo = repoVehiculo;
+            _repoTitular = repoTitular;
         }
 
-        public List<Tutlares> Ejecutar()
+        public List<Titular> Ejecutar()
         {
+            var listaTitutal = _repoTitular.ListarTitulares();
+            var listaVehiculo = _repoVehiculo.ListarVehiculos();
             //agarro una lista de titulares
+            //agarro la lista de autos
             //Por cada titular busco los autos con ese titular
-            return _repo.ListarTitularesCOnSusVehiculos();
+            //Puedo por cada titular recorrer todos los autos y buscar cual tiene este titular
+            //Hacer en el repo un metodo autos de un Titular -> me parece que esta es la opcion
+            return listaTitutal;
         }
     }
 }
