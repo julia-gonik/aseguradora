507a0e582de8fe75fa2e471e13d6c79d9a3f7c3a
Calvo <matias.calvo.2000@gmail.com>
2023-05-01 12:19:12 -0300

    Agrego pseudocodigo

--- Aseguradora.Aplicacion/IRepositorioVehiculo.cs
+++ Aseguradora.Aplicacion/IRepositorioVehiculo.cs
@@ -5,6 +5,7 @@ namespace Aseguradora.Aplicacion
         void AgregarVehiculo(Vehiculo vehiculo);
         void ModificarVehiculo(Vehiculo vehiculo);
         void EliminarVehiculo(int id);
+        List<Vehiculo> ListarVehiculosTitular(int id); //id del titular
         List<Vehiculo> ListarVehiculos();
     }
 }
--- Aseguradora.Aplicacion/ListarTitularesConSusVehiculosUseCase.cs
+++ Aseguradora.Aplicacion/ListarTitularesConSusVehiculosUseCase.cs
@@ -15,12 +15,8 @@ namespace Aseguradora.Aplicacion
         public List<Titular> Ejecutar()
         {
             var listaTitutal = _repoTitular.ListarTitulares();
-            var listaVehiculo = _repoVehiculo.ListarVehiculos();
-            //agarro una lista de titulares
-            //agarro la lista de autos
-            //Por cada titular busco los autos con ese titular
             //Puedo por cada titular recorrer todos los autos y buscar cual tiene este titular
-            //Hacer en el repo un metodo autos de un Titular -> me parece que esta es la opcion
+            //Hacer en el repo un metodo: autos de un Titular -> me parece que esta es la opcion
             return listaTitutal;
         }
     }
