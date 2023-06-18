17b952be9b170e979417072301c33c2b044f1bba
Calvo <matias.calvo.2000@gmail.com>
2023-06-17 23:26:04 -0300

    no anda intento usar delegado para hacer el chequeo

--- Aseguradora.Aplicacion/Entidades/Siniestro.cs
+++ Aseguradora.Aplicacion/Entidades/Siniestro.cs
@@ -10,20 +10,29 @@ public class Siniestro
     public string Direccion { get; set; } = "";
     public string Descripcion { get; set; } = "";
 
-    public Siniestro()
+    public Siniestro(ValidarUseCase validarUseCase, int polizaId)
     {
         FechaIngreso = DateTime.Now;
+
+        // Llamar al delegado para verificar si es válido
+        bool esValido = validarUseCase.Ejecutar(polizaId, FechaIngreso);
+
+        if (!esValido)
+        {
+            throw new SiniestroFueraDeVigenciaException("No es posible registrar el siniestro");
+        }
+        PolizaId = polizaId;
     }
 
     public override string ToString()
-{
-    return $"Siniestro Id: {Id}\n" +
-           $"PolizaId: {PolizaId}\n" +
-           $"Poliza: {Poliza?.ToString() ?? "N/A"}\n" +
-           $"Fecha de Ingreso: {FechaIngreso}\n" +
-           $"Fecha de Ocurrencia: {FechaOcurrencia}\n" +
-           $"Dirección: {Direccion}\n" +
-           $"Descripción: {Descripcion}";
-}
+    {
+        return $"Siniestro Id: {Id}\n" +
+               $"PolizaId: {PolizaId}\n" +
+               $"Poliza: {Poliza?.ToString() ?? "N/A"}\n" +
+               $"Fecha de Ingreso: {FechaIngreso}\n" +
+               $"Fecha de Ocurrencia: {FechaOcurrencia}\n" +
+               $"Dirección: {Direccion}\n" +
+               $"Descripción: {Descripcion}";
+    }
 
 }
