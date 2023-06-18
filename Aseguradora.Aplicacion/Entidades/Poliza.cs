e94cee6845c41533f5d29db6e92956b319f9e820
Julia Gonik <juligonik@alu.ing.unlp.edu.ar>
2023-06-18 15:42:20 -0300

    Formulario poliza

--- Aseguradora.Aplicacion/Entidades/Poliza.cs
+++ Aseguradora.Aplicacion/Entidades/Poliza.cs
@@ -1,25 +1,33 @@
+using System.ComponentModel.DataAnnotations;
 namespace Aseguradora.Aplicacion;
 
 public class Poliza
 {
-	public int Id { get; set; }
-	public int VehiculoId { get; set; }
-	public Vehiculo? Vehiculo { get; set; }
-	public double ValorAsegurado { get; set; }
-	public string Franquicia { get; set; } = "";
-	public TipoCobertura TipoCobertura { get; set; }
-	public DateTime FechaInicioVigencia { get; set; }
-	public DateTime FechaFinVigencia { get; set; }
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Vehiculo es necesario")]
+    public int VehiculoId { get; set; }
+    [Required]
+    public Vehiculo? Vehiculo { get; set; }
+    [Required]
+    public double ValorAsegurado { get; set; }
+    [Required]
+    public string Franquicia { get; set; } = "";
+    [Required]
+    public TipoCobertura TipoCobertura { get; set; }
+    [Required]
+    public DateTime FechaInicioVigencia { get; set; }
+    [Required]
+    public DateTime FechaFinVigencia { get; set; }
 
-	public override string ToString() 
-	{
-		return $"Poliza {Id} con franquicia {Franquicia}";
-	}
+    public override string ToString()
+    {
+        return $"Poliza {Id} con franquicia {Franquicia}";
+    }
 }
 
 
 public enum TipoCobertura
 {
-	ResponsabilidadCivil,
-	TodoRiesgo
+    ResponsabilidadCivil,
+    TodoRiesgo
 }
