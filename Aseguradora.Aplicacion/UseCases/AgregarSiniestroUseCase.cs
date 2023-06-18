d5283f9e283d4da53507e5df3a220dea3a7583ac
Julia Gonik <juligonik@alu.ing.unlp.edu.ar>
2023-06-18 18:30:10 -0300

    siniestro validado

--- Aseguradora.Aplicacion/UseCases/AgregarSiniestroUseCase.cs
+++ Aseguradora.Aplicacion/UseCases/AgregarSiniestroUseCase.cs
@@ -1,34 +1,32 @@
 namespace Aseguradora.Aplicacion
 {
-    public class AgregarSiniestroUseCase   
+    public class AgregarSiniestroUseCase
     {
         private readonly IRepositorioSiniestro _repoSiniestro;
         private readonly IRepositorioPoliza _repoPoliza;
-        
+
         public AgregarSiniestroUseCase(IRepositorioSiniestro repoSiniestro, IRepositorioPoliza repoPoliza)
         {
             _repoSiniestro = repoSiniestro;
             _repoPoliza = repoPoliza;
         }
-        
+
         public void Ejecutar(Siniestro siniestro)
         {
-            List<Poliza> lista = _repoPoliza.ListarPolizas();
-            bool valido = false;
-            var polizas = lista.Where(p => p.Id == siniestro.Id).ToList();
-            foreach(Poliza p in polizas){
-                if( siniestro.FechaOcurrencia >= p.FechaInicioVigencia && siniestro.FechaOcurrencia <= p.FechaFinVigencia)
-                {
-                    _repoSiniestro.AgregarSiniestro(siniestro);
-                    valido = true;
-                    break;
-                }
+            if (siniestro.PolizaId == 0) return;
+
+            Poliza? poliza = _repoPoliza.ObtenerPoliza(siniestro.PolizaId);
+
+            if (poliza == null) return;
+
+            if (siniestro.FechaOcurrencia >= poliza.FechaInicioVigencia && siniestro.FechaOcurrencia <= poliza.FechaFinVigencia)
+            {
+                _repoSiniestro.AgregarSiniestro(siniestro);
             }
-            if(valido == false){
+            else
+            {
                 throw new SiniestroFueraDeVigenciaException();
             }
-                
-            
         }
     }
 }
