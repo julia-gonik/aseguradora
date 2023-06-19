12e5f6148a07018fadcad414c148a64f75bd7135
Calvo <matias.calvo.2000@gmail.com>
2023-06-19 10:49:43 -0300

    Agrege el error en modificar

--- Aseguradora.Aplicacion/UseCases/ModificarSiniestroUseCase.cs
+++ Aseguradora.Aplicacion/UseCases/ModificarSiniestroUseCase.cs
@@ -2,16 +2,31 @@ namespace Aseguradora.Aplicacion
 {
     public class ModificarSiniestroUseCase   
     {
-        private readonly IRepositorioSiniestro _repo;
-        
-        public ModificarSiniestroUseCase(IRepositorioSiniestro repo)
+        private readonly IRepositorioSiniestro _repoSiniestro;
+        private readonly IRepositorioPoliza _repoPoliza;
+
+        public ModificarSiniestroUseCase(IRepositorioSiniestro repoSiniestro, IRepositorioPoliza repoPoliza)
         {
-            _repo = repo;
+            _repoSiniestro = repoSiniestro;
+            _repoPoliza = repoPoliza;
         }
         
         public void Ejecutar(Siniestro siniestro)
         {
-            _repo.ModificarSiniestro(siniestro);
+             if (siniestro.PolizaId == 0) return;
+
+            Poliza? poliza = _repoPoliza.ObtenerPoliza(siniestro.PolizaId);
+
+            if (poliza == null) return;
+
+            if (siniestro.FechaOcurrencia >= poliza.FechaInicioVigencia && siniestro.FechaOcurrencia <= poliza.FechaFinVigencia)
+            {
+                _repoSiniestro.ModificarSiniestro(siniestro);
+            }
+            else
+            {
+                throw new SiniestroFueraDeVigenciaException();
+            }
         }
     }
 }
